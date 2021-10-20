f3cd4964290e9a42232a01d4060d05803b3bdb40
heribertolord96 <heribertolord@gmail.com>
2021-10-20 16:41:17 -0500

    class 23: Manipulacion de arrays en mongp

---
+++ src/users/controllers/orders.controller.ts
@@ -0,0 +1,64 @@
+import {
+    Controller,
+    Get,
+    Param,
+    Post,
+    Body,
+    Put,
+    Delete,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import { OrdersService } from '../services/orders.service';
+import {
+    CreateOrderDto,
+    UpdateOrderDto,
+    AddProductsToOrderDto,
+} from '../dtos/order.dto';
+
+@ApiTags('orders')
+@Controller('orders')
+export class OrdersController {
+    constructor(private ordersService: OrdersService) { }
+
+    @Get()
+    findAll() {
+        return this.ordersService.findAll();
+    }
+
+    @Get(':id')
+    get(@Param('id') id: string) {
+        return this.ordersService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() payload: CreateOrderDto) {
+        return this.ordersService.create(payload);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() payload: UpdateOrderDto) {
+        return this.ordersService.update(id, payload);
+    }
+
+    @Put(':id/products')
+    updateProducts(
+        @Param('id') id: string,
+        @Body() payload: AddProductsToOrderDto,
+    ) {
+        return this.ordersService.addProducts(id, payload.productsIds);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.ordersService.remove(id);
+    }
+
+    @Delete(':id/product/:productId')
+    removeProduct(
+        @Param('id') id: string,
+        @Param('productId') productId: string,
+    ) {
+        return this.ordersService.removeProduct(id, productId);
+    }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { CustomerController } from './controllers/customers.controller';
-import { CustomersService } from './services/customers.service';
+import { CustomerController }       from './controllers/customers.controller';
+import { CustomersService }         from './services/customers.service';
 import { Customer, CustomerSchema } from './entities/customer.entity';
-import { UsersController } from './controllers/users.controller';
-import { UsersService } from './services/users.service';
-import { User, UserSchema } from './entities/user.entity';
-//import { OrdersController } from './controllers/orders.controller';
-//import { OrdersService } from './services/orders.service';
-//import { Order, OrderSchema } from './entities/order.entity';
-
-import { ProductsModule } from './../products/products.module';
+import { UsersController }          from './controllers/users.controller';
+import { UsersService }             from './services/users.service';
+import { User, UserSchema }         from './entities/user.entity';
+import { OrdersController }         from './controllers/orders.controller';
+import { OrdersService }            from './services/orders.service';
+import { Order, OrderSchema }       from './entities/order.entity';
+import { ProductsModule }           from './../products/products.module';
 
 @Module({
   imports: [
@@ -25,13 +24,13 @@ import { ProductsModule } from './../products/products.module';
         name: User.name,
         schema: UserSchema,
       },
-      /* {
+      {
         name: Order.name,
         schema: OrderSchema,
-      }, */
+      },
     ]),
   ],
-  controllers: [CustomerController, UsersController],
-  providers: [CustomersService, UsersService],
+  controllers: [CustomerController, UsersController, OrdersController],
+  providers: [CustomersService, UsersService, OrdersService],
 })
-export class UsersModule {}
+export class UsersModule { }
